4856c0dbe43b36aaa1036796449406ac07123eef
Mrigendra Soni <mrigendrasoni26@gmail.com>
2025-11-09 21:17:10 +0530

    Update FundMe.sol
    Adding withdraw function with some notes

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -2,9 +2,11 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+
+    using PriceConverter for uint256;
 
     uint256 public minUSD = 5*1e18; //can also be writte as 5e18 (1e18)
 
@@ -12,25 +14,36 @@ contract FundMe {
     mapping(address funder => uint256 amtFunded) public addressToAmount;
 
     function fund() public payable{
-        require(getConversionRate(msg.value) > minUSD, "Not enough funds");
+        require(msg.value.getConversionRate() >= minUSD, "Not enough funds");
         listOfSenders.push(msg.sender);
         addressToAmount[msg.sender] = addressToAmount[msg.sender] + msg.value;
     }
 
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        //Price of ETH in terms of USD
-        return uint256(price * 1e10); // We do this because msg.value has 18 decimal places and this `price` var has 8, so we multiply by 1e10 to make them equal
-    }
+    function withdraw() public {
+        //for loop
+        // 
+        for(uint256 funderIndex=0; funderIndex<listOfSenders.length; funderIndex++) {
+            address funder = listOfSenders[funderIndex];
+            addressToAmount[funder]=0;
+        }
+        //reset the array
+        listOfSenders = new address[](0);
+        //withdraw the funds
+      
+        //msg.address = address
+        //payable(msg.sender) = payable address
+        // transfer - errors out
+        payable(msg.sender).transfer(address(this).balance);
+        // send - reverts with a boolean
+        bool success = payable(msg.sender).send(address(this).balance);
+        require(success, "Send failed");
+        // call - allows us to make a call and send msg.value, and returns 2 params
+        // bool points if the call was successful or not, returned bytes saves any data returned by that function
+        // since bytes objest is an array, it needs to be stored in memory
+        (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance("");
+        // we can leave the second var blank, saying that we know it returns this data but we don't need it
+        require(callSuccess, "Call failed");
+        
 
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; //multiply first because solidity doesn't support decimals, and returns whole numbers so calculations can go wrong
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns (uint256) {
-        return (AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306)).version();
     }
 }
